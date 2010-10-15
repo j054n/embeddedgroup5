09ea75a56108bb4597332019e393d4db9450fc28
davis.ty.r <davis.ty.r@7951f41f-f6fc-732a-121d-bd500b484b68>
2010-10-15 21:11:21 +0000

    Changes for p18lf27j13

--- trunk/milestone2p4/user_interrupts.c
+++ trunk/milestone2p4/user_interrupts.c
@@ -12,8 +12,7 @@ void initADC()
 {
 	OpenADC(ADC_FOSC_8 & ADC_RIGHT_JUST & ADC_0_TAD,
 		ADC_CH0 & ADC_CH1 &
-		ADC_INT_OFF & ADC_VREFPLUS_VDD & 
-		ADC_VREFMINUS_VSS, 0b1011);
+		ADC_INT_OFF & ADC_REF_VDD_VSS, ADC_TRIG_CCP2, 0b11111000);
 	// Use SetChanADC(ADC_CH1) to look at sensor channel
 	SetChanADC(ADC_CH1);
 	Delay10TCYx( 50 );
